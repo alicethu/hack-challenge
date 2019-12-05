23289a0440500875da4134c0e2cbad02e864068a
Michelle-Wu11 <szwuyunan24@gmail.com>
2019-12-04 22:49:53 -0500

    Add files via upload

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 import json
 from db import db, Spot, User
 from flask import Flask, request
+from datetime import datetime
+
 
 db_filename = "hack_challenge.db"
 app = Flask(__name__)
@@ -38,15 +40,70 @@ def create_user():
 def create_spot():
     post_body = json.loads(request.data)
     name = post_body['name']
-    #tags = post_body['tags']
+    tagsString = post_body['tags']
+    opening = post_body['opening']
+    closing = post_body['closing']
+    imageurl = post_body['imageurl']
+    #determine whether it is opening or closing based on current time
+    nowHour = datetime.now().hour
+    nowMin = datetime.now().min
+    if nowHour>int(opening[0:opening.find(':')]) or (nowHour==int(opening[0:openin.find(':')]) and nowMin >= int(opening[opening.find(':')+1:])):
+        if nowHour<int(closing[0:closing.find(':')]) or (nowHour==int(closing[0:closing.find(':')]) and nowMin <= int(closing[closing.find(':')+1:])):
+            isopening = True
+        else:
+            isopening = False
+    else:
+        isopening = False
+    tags = [x.strip() for x in tagsString.split(',')]
     spot = Spot(
         name = name,
-        numOfFavorited = 0
-        #tags = tags
+        numOfFavorited = 0,
+        opening = opening,
+        closing = closing,
+        isopening = isopening,
+        imageurl = imageurl
     )
+    for t in tags:
+        spot.tags.append(t)
     db.session.add(spot)
     db.session.commit()
     return json.dumps({'success': True, 'data': spot.serialize()}), 201
+
+#user adds a favorite spot
+@app.route('/api/user/<int:user_id>/favorite/', methods=['POST'])
+def create_favorite(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        return json.dumps({'success': False, 'error': 'User not found!'}), 404
+    post_body = json.loads(request.data)
+    spot_id = post_body.get('spot_id')
+    favorite = Spot.query.filter_by(id=spot_id).first()
+    if not favorite:
+        return json.dumps({'success': False, 'error': 'Spot not found!'}), 404
+    user.favorites.append(favorite)
+    #favorite.users.append(user)   #something's wrong with this
+    favorite.numOfFavorited += 1
+    db.session.add(favorite)
+    db.session.commit()
+    return json.dumps({'success': True, 'data': user.serialize()}), 201  #depends on whether you want the added spot or user added to be serialized
+
+#user deletes a favorite spot
+@app.route('/api/user/<int:user_id>/favorite/', methods=['DELETE'])
+def remove_favorite(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        return json.dumps({'success': False, 'error': 'User not found!'}), 404
+    post_body = json.loads(request.data)
+    spot_id = post_body.get('spot_id')
+    favorite = Spot.query.filter_by(id=spot_id).first()
+    if not favorite:
+        return json.dumps({'success': False, 'error': 'Spot not found!'}), 404
+    user.favorites.remove(favorite)
+    #favorite.users.remove(user)    #something's wrong with this
+    favorite.numOfFavorited -= 1
+    db.session.commit()
+    return json.dumps({'success': True, 'data': user.serialize()}), 201  #depends on whether you want the deleted spot or user added to be serialized
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
--- src/db.py
+++ src/db.py
@@ -2,20 +2,37 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+favorites_table = db.Table('favorite', db.Model.metadata,
+    db.Column('spot_id', db.Integer, db.ForeignKey('spot.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False)
-    favorites = db.Column(db.Integer, db.ForeignKey('spot.id'))
+    favorites = db.relationship('Spot', secondary=favorites_table, back_populates='users')
 
     def __init__(self, **kwargs):
         self.username = kwargs.get('username', '')
-        self.favorites = kwargs.get('favorites', -1)
+        self.favorites = []
 
     def serialize(self):
+        favorites = []
+        for f in self.favorites:
+            favorite = {
+                'id': f.id,
+                'name': f.name,
+                'numOfFavorited': f.numOfFavorited,
+                'tags': f.tags
+            }
+            favorites.append(favorite)
+
         return{
             'id': self.id,
             'username': self.username,
+            'favorites': favorites
         }
 
 class Spot(db.Model):
@@ -23,17 +40,32 @@ class Spot(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     numOfFavorited = db.Column(db.Integer, nullable=False)
-    tags = []
+    users = db.relationship('User', secondary=favorites_table, back_populates='favorites')
+    tags = db.Column(db.PickleType, nullable=True)
+    opening = db.Column(db.String, nullable=False)
+    closing = db.Column(db.String, nullable=False)
+    isopening = db.Column(db.Boolean, nullable=False)
+    imageurl = db.Column(db.String, nullable=False)
 
     def __init__(self, **kwargs):
         self.name = kwargs.get('name', '')
-        self.numOfFavorited = kwargs.get('numOfFavorited', -1)
-        self.tags = kwargs.get('tags', '')
+        self.numOfFavorited = kwargs.get('numOfFavorited', 0)
+        self.users = []
+        self.tags = []
+        self.opening = kwargs.get('opening', '')
+        self.closing = kwargs.get('closing', '')
+        self.imageurl = kwargs.get('imageurl', '')
+        self.isopening = kwargs.get('isopening', True)
+
 
     def serialize(self):
         return{
             'id': self.id,
             'name': self.name,
             'numOfFavorited': self.numOfFavorited,
-            'tags': self.tags
+            'tags': self.tags,
+            'opening': self.opening,
+            'closing': self.closing,
+            'isopening': self.isopening,
+            'imageurl': self.imageurl
         }
